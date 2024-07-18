abba94f56c9befb2258c2f1c29d912906b3919c6
uzairamanat <uzair.amanat@hotmail.com>
2024-07-18 18:46:41 +0100

    creating top navbar with dark mode toggle

--- src/App.js
+++ src/App.js
@@ -1,9 +1,29 @@
-
+import { CssBaseline, ThemeProvider } from "@mui/material";
+import { createTheme } from "@mui/material";
+import { useMemo } from "react";
+import { useSelector } from "react-redux";
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
+import state from "state";
+import { themeSettings } from "theme";
+import Dashboard from "pages/dashboard";
+import Layout from "pages/Layout";
 
 function App() {
+  const mode = useSelector ((state) => state.global.mode);
+  const theme = useMemo(() => createTheme(themeSettings(mode)), [mode]);
   return (
     <div className="app">
-      
+      <BrowserRouter>
+        <ThemeProvider theme={theme}>
+          <CssBaseline/>
+          <Routes>
+            <Route element= {<Layout />}>
+              <Route path="/" element = {<Navigate to = "/dashboard" replace />} />
+              <Route path ="/dashboard" element= {<Dashboard />} />
+            </Route>
+          </Routes>
+        </ThemeProvider>
+      </BrowserRouter>
     </div>
   );
 }
