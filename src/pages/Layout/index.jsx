b6fc8d803312311ff5e7e090517b134c85d7f02c
uzairamanat <uzair.amanat@hotmail.com>
2024-07-21 19:07:40 +0100

    completed sidebar

--- src/pages/Layout/index.jsx
+++ src/pages/Layout/index.jsx
@@ -1,19 +1,28 @@
-import { useState } from "react";
-import React from 'react';
+import React, { useState } from 'react';
 import { Outlet } from "react-router-dom";
-import { useSelector } from "react-redux";
 import { Box, useMediaQuery } from "@mui/material";
 import Navbar from "components/Navbar";
+import Sidebar from "components/Sidebar";
 
 const Layout = () => {
+  const isNonMobile = useMediaQuery("(min-width: 600px)");
+  const [isSidebarOpen, setIsSidebarOpen] = useState(true);
+
   return (
-    <Box width="100%" height="100%">
-      <Box>
-        <Navbar />
+    <Box display={isNonMobile ? "flex" : "block"} width="100%" height="100%">
+      <Sidebar
+        isNonMobile={isNonMobile}
+        drawerWidth="250px"
+        isSidebarOpen={isSidebarOpen}
+        setIsSidebarOpen={setIsSidebarOpen}
+      />
+      <Box flexGrow={1}>
+        <Navbar isSidebarOpen={isSidebarOpen} setIsSidebarOpen={setIsSidebarOpen} />
         <Outlet />
       </Box>
     </Box>
-  )
-}
+  );
+};
 
 export default Layout;
+
